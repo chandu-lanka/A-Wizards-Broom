c832c74263491bf52af3e95d455e5ac6cf74f480
TriangleDevv <chandu.lanka13@gmail.com>
2021-09-09 21:21:44 +0530

    ⚡Got A Minimalistic Game Class Up And Running ;)

---
+++ src/Game.cpp
@@ -0,0 +1,55 @@
+#include "Game.hpp"
+
+Game::Game() {}
+Game::~Game() {}
+
+// functions
+void Game::init(const char* title, int x, int y, int width, int height, bool fullScreen) {
+    int flags = 0;
+
+    if (fullScreen) {
+        flags = SDL_WINDOW_FULLSCREEN;
+    }
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        std::cout << "SDL2 Initialization Successful" << std::endl;
+        window = SDL_CreateWindow(title, x, y, width, height, flags);
+        if (window) {
+            std::cout << "SDL2 Window Successfully Created" << std::endl;
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if (renderer) {
+            SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+            std::cout << "SDL2 Renderer Successfully Created" << std::endl;
+        }
+
+        isRunning = true;
+    } else {
+        isRunning = false;
+    }
+}
+
+void Game::handleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+    }
+}
+
+void Game::update() {}
+
+void Game::render() {
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Game::clean() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_QUIT;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include "Game.hpp"
+
+Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
-    std::cout << "sdl-boilerplate is working..." << std::endl;
-    std::cin.get();
-    
+    game = new Game();
+    game->init("A Wizards Broom", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, false);
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+
+    game->clean();
+
     return 0;
 }
